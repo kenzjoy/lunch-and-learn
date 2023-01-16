e6c339ea761242e0470d8680895b133a301483eb
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-16 12:22:52 -0700

    edamam service tests passing

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      post '/favorites', to: 'favorites#update'
-      get '/recipes', to: 'recipes#index'
-      get '/learning_resources', to: 'learning_resources#show'
-      # or get '/learning_resources/country/:id', to: 'learning_resources#show' ??
-      post '/users', to: 'users#update'
+      resources :recipes, only: [:index]
+      resources :favorites, only: [:create, :index]
+
+      # post '/favorites', to: 'favorites#update'
+      # get '/learning_resources', to: 'learning_resources#show'
+      # post '/users', to: 'users#update'
 
     end
   end
---
+++ spec/services/edamam_service_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe EdamamService do
+  it 'returns recipes from a given country' do
+    country = 'thailand'
+    recipes = EdamamService.get_recipe_search(country)
+
+    expect(recipes[:hits]).to be_an(Array)
+    expect(recipes[:hits].count).to eq(20)
+
+    r = recipes[:hits].first
+
+    expect(r.keys).to eq([:recipe, :_links])
+    expect(r[:recipe].keys).to include(
+      :uri,
+      :label,
+      :image,
+      :images,
+      :source,
+      :url,
+      :shareAs,
+      :yield,
+      :dietLabels,
+      :healthLabels,
+      :cautions,
+      :ingredientLines,
+      :ingredients,
+      :calories,
+      :totalWeight,
+      :totalTime,
+      :cuisineType,
+      :mealType,
+      :dishType,
+      :totalNutrients,
+      :totalDaily,
+      :digest
+      )
+
+    expect(r[:_links][:self].keys).to include(
+      :href,
+      :title
+      )
+  end
+end
