b033d0dc4df29f5d495a6e990f0783973e4113cd
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-15 21:20:07 -0700

    adds all non-extension endpoints in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      post '/favorites', to: 'favorites#update'
+      get '/recipes', to: 'recipes#index'
+      get '/learning_resources', to: 'learning_resources#show'
+      # or get '/learning_resources/country/:id', to: 'learning_resources#show' ??
+      post '/users', to: 'users#update'
+
+    end
+  end
 end
