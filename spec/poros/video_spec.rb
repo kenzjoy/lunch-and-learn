9d0d771cfd38ee4167c4f6bb52a7909b176eb27e
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-17 16:23:23 -0700

    creates learning resources serializer

--- spec/poros/video_spec.rb
+++ spec/poros/video_spec.rb
@@ -5,13 +5,18 @@ RSpec.describe Video, :vcr do
     country = 'new zealand'
     info = {
       snippet: { title: 'A Super Quick History of New Zealand' },
-      id: { youtube_video_id: 'm6pVgnWaGsk' }
+      id: { videoId: 'm6pVgnWaGsk' }
     }
     @video = Video.new(info, country)
   end
 
   it 'exists' do
     expect(@video).to be_a(Video)
+  end
 
+  it 'has attributes' do
+    expect(@video.country).to eq('new zealand')
+    expect(@video.title).to eq('A Super Quick History of New Zealand')
+    expect(@video.youtube_video_id).to eq('m6pVgnWaGsk')
   end
 end
