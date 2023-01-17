6de41f7272a80a97c6bbfdaccb5980ca773e087c
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-17 12:31:12 -0700

    video response passing test

--- app/services/video_service.rb
+++ app/services/video_service.rb
@@ -1,3 +1,19 @@
 class VideoService
-  
+  def self.conn
+    Faraday.new(url: "https://www.googleapis.com") do |f|
+      f.params['key'] = ENV['youtube_api_key']
+    end
+  end
+
+  def self.get_video(country)
+    response = conn.get('/youtube/v3/search') do |search|
+      search.params['part'] = 'snippet'
+      search.params['channelId'] = 'UCluQ5yInbeAkkeCndNnUhpw'
+      search.params['maxResults'] = 1
+      search.params['type'] = 'video'
+      search.params['q'] = country
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/video_service_spec.rb
+++ spec/services/video_service_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe VideoService, :vcr do
-  
+  it 'returns a json response with a video from the Mr. History channel about a given country' do
+    country = 'egypt'
+    videos = VideoService.get_video(country)
+
+    expect(videos).to be_a(Hash)
+    expect(videos).to have_key(:items)
+
+    video = videos[:items].first
+
+    expect(video).to have_key(:id)
+    expect(video[:id]).to have_key(:videoId)
+    expect(video[:id][:videoId]).to be_a(String)
+    expect(video).to have_key(:snippet)
+    expect(video[:snippet]).to have_key(:title)
+    expect(video[:snippet][:title]).to be_a(String)
+  end
 end
