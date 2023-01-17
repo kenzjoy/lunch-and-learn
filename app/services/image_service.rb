567908166193109381661ca28f9c9085309b7168
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-17 13:12:31 -0700

    photo connection

--- app/services/image_service.rb
+++ app/services/image_service.rb
@@ -1,3 +1,15 @@
 class ImageService
-  
+  def self.conn
+    Faraday.new(url: "https://api.unsplash.com") do |f|
+      f.params['client_id'] = ENV['unsplash_access_key']
+    end
+  end
+
+  def self.get_photos(country)
+    response = conn.get('search/photos') do |search|
+      search.params['query'] = country
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe ImageService, :vcr do
-  
+  it 'returns a json response with images related to the country given in the search query' do
+    country = 'sweden'
+    photos = ImageService.get_photos(country)
+
+    expect(photos).to be_a(Hash)
+    expect(photos).to have_key(:results)
+
+    photo = photos[:results].first
+
+    expect(photo).to have_key(:alt_description)
+    expect(photo[:alt_description]).to be_a(String)
+    expect(photo).to have_key(:urls)
+    expect(photo[:urls].keys).to include(
+      :raw,
+      :full,
+      :regular,
+      :small,
+      :thumb, 
+      :small_s3
+    )
+    expect(photo[:urls][:regular]).to be_a(String)
+  end
+
 end
