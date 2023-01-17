119c2767bba2851e85a43a28e29437c9d2459047
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-16 17:48:18 -0700

    set up recipe search test for edamam facade

--- app/facades/edamam_facade.rb
+++ app/facades/edamam_facade.rb
@@ -1,3 +1,5 @@
 class EdamamFacade
-  
+  def self.recipe_search(country)
+    country_recipes = EdamamService.get_recipe_search(country)
+  end
 end
--- spec/facades/edamam_facade_spec.rb
+++ spec/facades/edamam_facade_spec.rb
@@ -1,2 +1,12 @@
 require 'rails_helper'
 
+RSpec.describe EdamamFacade, :vcr do
+  describe '#recipe_search(country)' do
+    it 'returns a list of recipes from the given country' do
+      country = 'wales'
+      recipes = EdamamFacade.recipe_search(country)
+
+      expect(recipes).to be_an(Array)
+    end
+  end
+end
