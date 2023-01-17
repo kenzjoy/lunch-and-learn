cbd1779e5cd72e11aa82785f27b71522a94e7ba5
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-17 09:29:50 -0700

    adds learning resources routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :recipes, only: [:index]
-      resources :favorites, only: [:create, :index]
+      resources :learning_resources, only: [:index]
 
       # post '/favorites', to: 'favorites#update'
       # get '/learning_resources', to: 'learning_resources#show'
