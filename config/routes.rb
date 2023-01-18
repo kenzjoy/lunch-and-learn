5e03dba222e218bc47f0f9473375163eddb9c429
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-17 19:27:18 -0700

    creates test in user model spec to validate attributes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :recipes, only: [:index]
       resources :learning_resources, only: [:index]
+      resources :users, only: [:create]
 
       # post '/favorites', to: 'favorites#update'
       # get '/learning_resources', to: 'learning_resources#show'
