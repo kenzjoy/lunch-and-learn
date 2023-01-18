9d9f06449413b5e0ababfbb80efb50ce2896f140
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-17 21:42:26 -0700

    status 201 passing)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user), status: 201
+    else
+      render json: { error: 'Email is already registered.' }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :api_key)
   end
 end
--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -3,14 +3,27 @@ require 'rails_helper'
 RSpec.describe 'users create' do
   it 'creates a new user and generates an api key for said user' do
     headers = { "CONTENT_TYPE" => "application/json" }
-    user_params = {
+    body = JSON.generate(
       'name': 'uncle ebenezer',
       'email': 'tweezer@badboyfreecat.com'
-    }
+    )
 
-    post '/api/v1/users', headers: headers, params: JSON.generate(user: user_params)
-require 'pry'; binding.pry
+    post '/api/v1/users', headers: headers, params: body
+
     expect(response).to be_successful
     expect(response.status).to eq(201)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed).to have_key(:data)
+    expect(parsed[:data]).to have_key(:id)
+    expect(parsed[:data]).to have_key(:type)
+    expect(parsed[:data]).to have_key(:attributes)
+    expect(parsed[:data][:type]).to eq('user')
+    expect(parsed[:data][:attributes].keys).to include(
+      :name, 
+      :email,
+      :api_key
+    )
   end
 end
