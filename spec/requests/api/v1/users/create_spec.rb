78b469995d25d98588366112ba97a02b53c4a2e0
Mackenzie Leng <kenz.joy@outlook.com>
2023-01-19 18:17:44 -0700

    adds correct error message for already used email

--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -48,4 +48,32 @@ RSpec.describe 'users create' do
       # expect(parsed[:error]).to eq('Please try again.')
     end
   end
+
+  describe 'when an email has already been taken' do
+    it 'returns an error message' do
+      headers = { "CONTENT_TYPE" => "application/json" }
+      body = JSON.generate(
+        'name': 'uncle ebenezer',
+        'email': 'tweezer@badboyfreecat.com'
+      )
+
+      post '/api/v1/users', headers: headers, params: body
+
+      headers_2 = { "CONTENT_TYPE" => "application/json" }
+      body_2 = JSON.generate(
+        'name': 'mike gordon',
+        'email': 'tweezer@badboyfreecat.com'
+      )
+
+      post '/api/v1/users', headers: headers_2, params: body_2
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(400)
+      
+      parsed = JSON.parse(response.body, symbolize_names: true)
+      expect(parsed).to be_a(Hash)
+      expect(parsed).to have_key(:email)
+      expect(parsed[:email]).to eq(["has already been taken"])
+    end
+  end
 end
